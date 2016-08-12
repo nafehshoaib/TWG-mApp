986542c03d204bb21ee74c003e27cea4936a22fb
Your Name <nafehshoaib14@gmail.com>
2016-08-12 16:30:58 -0400

    Took out MGLMapView Added MKMapView Added Initial Code for the mapView Created Office class Added MapLocal.plist

--- mApp/ViewController.swift
+++ mApp/ViewController.swift
@@ -8,15 +8,28 @@
 
 import Cocoa
 import Quartz
+import MapKit
 
 class ViewController: NSViewController {
     
-    @IBOutlet var pdfView: PDFView?
-    var doc: PDFDocument = PDFDocument(URL: NSBundle.mainBundle().URLForResource("Floorplan_2016.06.25.png", withExtension: "pdf"))
+    //@IBOutlet var pdfView: PDFView?
+    //var doc: PDFDocument = PDFDocument(URL: NSBundle.mainBundle().URLForResource("Floorplan_2016.06.25.png", withExtension: "pdf"))
+    @IBOutlet var mapView: MKMapView?
+    var office = Office(filename: "MapLocal")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        pdfView?.setDocument(doc)
+        //pdfView?.setDocument(doc)
+        
+        let latDelta = office.overlayTopLeftCoordinate.latitude -
+            office.overlayBottomRightCoordinate.latitude
+        
+        // think of a span as a tv size, measure from one corner to another
+        let span = MKCoordinateSpanMake(fabs(latDelta), 0.0)
+        
+        let region = MKCoordinateRegionMake(office.midCoordinate, span)
+        
+        mapView!.region = region
         // Do any additional setup after loading the view.
     }
 
@@ -27,5 +40,11 @@ class ViewController: NSViewController {
     }
 
 
+}
+
+// MARK: - Map View delegate
+
+extension ViewController: MKMapViewDelegate {
+    
 }
 
