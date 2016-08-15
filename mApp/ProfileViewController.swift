7134c72d5e983b9f18e4ef9994f0735f01af9c41
TwinkleMehta <twinklemehta8@gmail.com>
2016-08-15 15:17:10 -0400

    Added profile view controller

---
+++ mApp/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  mApp
+//
+//  Created by Mehta on 2016-08-15.
+//  Copyright © 2016 nafehshoaib. All rights reserved.
+//
+
+import Cocoa
+
+class ProfileViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
