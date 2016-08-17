cec6b3a9699c9b5f6b1c2498f3ef6416e39344e2
Your Name <Nafeh Shoaib>
2016-08-17 15:13:29 -0400

    Added TWG MapView Made loginViewController show up as sheet in ViewController.swift

---
+++ mApp/TWGMapView.swift
@@ -0,0 +1,19 @@
+//
+//  TWGMapView.swift
+//  mApp
+//
+//  Created by Nafeh Shoaib on 2016-08-17.
+//  Copyright © 2016 nafehshoaib. All rights reserved.
+//
+
+import Cocoa
+import MapKit
+
+class TWGMapView: MKMapView {
+
+    override func drawRect(dirtyRect: NSRect) {
+        super.drawRect(dirtyRect)
+        // Drawing code here.
+    }
+    
+}
